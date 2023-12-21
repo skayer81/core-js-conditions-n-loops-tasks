16eabb55b793c2621eddc4848e26892680922012
Sergey <seleznev-krista@mail.ru>
2023-12-21 04:49:27 +0300

    feat : add isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -215,8 +215,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const char = String(digit);
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] === char) return true;
+    i += 1;
+  }
+  return false;
 }
 
 /**
